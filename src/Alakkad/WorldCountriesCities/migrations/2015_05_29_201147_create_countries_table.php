eabf8867753b0fb9bfda06bf8c38ba096ba610f2
Vedmant <vedmant@users.noreply.github.com>
2015-08-04 11:08:23 +0300

    Fixed migration

--- src/Alakkad/WorldCountriesCities/migrations/2015_05_29_201147_create_countries_table.php
+++ src/Alakkad/WorldCountriesCities/migrations/2015_05_29_201147_create_countries_table.php
@@ -18,17 +18,17 @@ class CreateCountriesTable extends Migration
             $table->enum('continent', ['Asia', 'Europe', 'North America', 'Africa', 'Oceania', 'Antarctica', 'South America']);
             $table->string('region', 26);
             $table->decimal('surface_area', 10, 2);
-            $table->integer('indep_year', 4);
-            $table->integer('population', 11)->unsigned();
+            $table->smallInteger('indep_year');
+            $table->integer('population')->unsigned();
             $table->decimal('life_expectancy', 3, 1);
             $table->decimal('gnp', 10, 2);
             $table->decimal('gnp_old', 10, 2);
             $table->string('local_name', 45);
             $table->string('government_form', 45);
             $table->string('head_of_state', 60);
-            $table->integer('capital', 11)->unsigned();
+            $table->integer('capital')->unsigned();
             $table->string('code2', 2);
-            $table->foreign('capital', 11)->references('id')->on('cities');
+            $table->foreign('capital')->references('id')->on('cities');
         });
     }
 
