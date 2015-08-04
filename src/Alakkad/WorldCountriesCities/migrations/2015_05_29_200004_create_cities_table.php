90660c735ad9dc2cff0ad03db1b7852f9a5ef226
Vedmant <vedmant@users.noreply.github.com>
2015-08-04 11:23:47 +0300

    Update 2015_05_29_200004_create_cities_table.php

--- src/Alakkad/WorldCountriesCities/migrations/2015_05_29_200004_create_cities_table.php
+++ src/Alakkad/WorldCountriesCities/migrations/2015_05_29_200004_create_cities_table.php
@@ -14,7 +14,7 @@ class CreateCitiesTable extends Migration
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('name', 35);
-            $table->string('country_code', 3);
+            $table->string('country_code', 3)->index();
             $table->string('district', 20);
             $table->integer('population')->unsigned();
         });
